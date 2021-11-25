0d54f8ac0c5fd529b44aa1605e9a6efd0157f86b
Kirhaz <johndoe@example.com>
2021-11-26 02:05:51 +0300

    second commit

--- services/index.js
+++ services/index.js
@@ -201,7 +201,7 @@ export const getCategoryPost = async (slug) => {
             slug
             title
             excerpt
-            featuredImage {
+            postImage {
               url
             }
             categories {
